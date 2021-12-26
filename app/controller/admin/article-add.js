b5b25a5924c83c9d9f0396eca48b5710a8c4ae11
chenjy <chenjy@email.com>
2021-12-26 20:52:17 +0800

    submit article is ok

--- app/controller/admin/article-add.js
+++ app/controller/admin/article-add.js
@@ -7,7 +7,7 @@ module.exports = (req, res) => {
     //创建表单解析对象
     const form = new formidable.IncomingForm();
     //配置上传文件的存放位置
-    form.uploadDir = path.join(__dirname, '../', '../', 'public', 'uploads');
+    form.uploadDir = path.join(__dirname, '../', '../', '../', 'public', 'uploads');
     //保留上传文件的后缀
     form.keepExtensions = true;
     //解析表单
@@ -15,14 +15,16 @@ module.exports = (req, res) => {
             // 1.err错误对象 如果表单解析失败 err里面存储错误信息 如果表单解析成功 err将会是null
             // 2.fields 对象类型 保存普通表单数据
             // 3.files 对象类型 保存了和上传文件相关的数据
-            // res.send(files.cover.path.split('public')[1])
+            //res.send(files.cover.filepath.split('public')[1])
+
             await Article.create({
                 title: fields.title,
                 author: fields.author,
                 publishDate: fields.publishDate,
-                cover: files.cover.path.split('public')[1],
+                cover: files.cover.filepath.split('public')[1],
                 content: fields.content,
             });
+
             //将页面重定向到文章列表页面
             res.redirect('/admin/article');
         })
